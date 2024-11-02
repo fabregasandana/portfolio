139de4d5a87c57d749e5217bbf02d824c09123f4
fabregasandana <fabregasandana02106@gmail.com>
2024-11-02 20:02:05 +0700

    finish landing page

---
+++ public/js/typed.js
@@ -0,0 +1,9 @@
+var typed = new Typed("#element", {
+    strings: ["Programmer", "Designer", "UI UX Designer"],
+    typeSpeed : 100,
+    backSpeed : 100,
+    looped : true,
+    showCursor: true,
+  cursorChar: '|',
+  autoInsertCss: true,
+})
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html, js}", "index.html", "tes.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
